0e26790f408f74fe061ac82b1f13cbf84d200c48
joeluna17 <joeluna17@yahoo.com>
2021-09-19 14:38:00 -0500

    adding yml file

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,6 @@ module.exports = {
     "gatsby-plugin-netlify-cms",
     "gatsby-plugin-styled-components",
     "gatsby-plugin-image",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
